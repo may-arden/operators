bc4a91f4533050c3d9836028e03dbfe165e55c04
may arden <mayard3n@gmail.com>
2020-10-21 18:29:00 -0700

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,11 @@
 def unsafe?(speed)
-    # speed > 60 || speed < 40 ? true : false
-    # speed <= 60 && speed >= 40 ? false : true
+    if speed > 60
+        true
+    elsif speed < 40
+        true
+    else speed <= 60 && speed >+ 40
+        false
+    end
 end
 
 
